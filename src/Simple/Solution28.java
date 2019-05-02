bd151a64e91f97b397676650d448b1cb37d8c162
fakeache <1650143222@qq.com>
2019-05-02 18:38:22 +0800

    28

---
+++ src/Simple/Solution28.java
@@ -0,0 +1,29 @@
+package Simple;
+
+import java.util.Arrays;
+
+class Solution28 {
+    public static int strStr(String haystack, String needle) {
+        if(needle==null || needle.length()==0){
+            return 0;
+        }
+        int nlen=needle.length();
+        int hlen=haystack.length();
+        for(int i=0;i<=hlen-nlen;i++){
+            for(int j=0;j<nlen;j++){
+                if(haystack.charAt(i+j)!=needle.charAt(j)){
+                    break;
+                }
+                if(j==nlen-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        String a = "hello";
+        String b = "lo";
+        System.out.println(strStr(a, b));
+    }
+}
