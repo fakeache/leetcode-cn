f89c12b1b66496a5f8849aa5c034b7a63fe1207c
fakeache <1650143222@qq.com>
2019-07-09 12:45:20 +0800

    100

--- src/Simple/Solution100.java
+++ src/Simple/Solution100.java
@@ -1,20 +1,14 @@
 package Simple;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+import java.util.LinkedList;
+
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
 }
+
 public class Solution100 {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null || q==null){
@@ -31,5 +25,32 @@ public class Solution100 {
         else{
             return false;
         }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        LinkedList<TreeNode> list = new LinkedList<>();
+        list.add(root);
+        list.add(root);
+        while(!list.isEmpty()){
+            TreeNode t1 = list.poll();
+            TreeNode t2 = list.poll();
+            if(t1==null && t2==null){
+                continue;
+            }
+            if(t1==null || t2==null || t1.val!=t2.val){
+                return false;
+            }
+            list.add(t1.left);
+            list.add(t2.right);
+            list.add(t1.right);
+            list.add(t2.left);
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        LinkedList<TreeNode> l = new LinkedList<>();
+        l.add(null);
+        System.out.println(l.size());
     }
 }
