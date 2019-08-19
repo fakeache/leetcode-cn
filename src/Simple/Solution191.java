6aa4c3a6bc3c2f90b10a0d1f49b991360fe4166b
fakeache <1650143222@qq.com>
2019-08-19 21:21:07 +0800

    191

---
+++ src/Simple/Solution191.java
@@ -0,0 +1,17 @@
+package Simple;
+
+public class Solution191 {
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+        int sum = 0;
+        int t = n;
+        while(t!=0){
+            sum++;
+            t = t & (t-1);
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(-585));
+    }
+}
