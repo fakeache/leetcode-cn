b6e35b90d26aedd9521944568ecb79d728b390b9
fakeache <1650143222@qq.com>
2019-09-10 22:18:10 +0800

    198

--- src/Simple/Solution191.java
+++ src/Simple/Solution191.java
@@ -13,5 +13,6 @@ public class Solution191 {
     }
     public static void main(String[] args) {
         System.out.println(hammingWeight(-585));
+
     }
 }
---
+++ src/Simple/Solution198.java
@@ -0,0 +1,32 @@
+package Simple;
+
+import java.util.Arrays;
+
+public class Solution198 {
+    public static int rob(int[] nums) {
+        if (nums==null || nums.length == 0) {
+            return 0;
+        }
+        int a = nums[0];
+        if (nums.length == 1) {
+            return a;
+        }
+        if (nums.length == 2) {
+            int b = Math.max(nums[0], nums[1]);
+            return b;
+        }
+        int b = Math.max(nums[0], nums[1]);
+        int c = 0;
+        for (int i = 2; i < nums.length; i++) {
+            c = Math.max(b, a + nums[i]);
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{2, 1, 1, 2};
+        System.out.println(rob(nums));
+    }
+}
